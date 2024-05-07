cae1ee177245e94b2869b43fa306b7d320051ecb
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-05-07 14:01:25 +0530

    added a Scrolling product section

---
+++ bestSellingProducts.js
@@ -0,0 +1,34 @@
+let bestSellingProductsContainer = document.querySelector(
+  ".bestselling-products"
+);
+let templateBestsellingProducts = document.querySelector(
+  "#template-bestselling-products"
+);
+
+export let showBestSellingProduct = (bestSellingProducts) => {
+  if (!bestSellingProducts) {
+    return false;
+  }
+  bestSellingProducts.forEach((bestSellingProducts) => {
+    let { id, image, price } = bestSellingProducts;
+    let cloneImageOfBestSellingProducts = document.importNode(
+      templateBestsellingProducts.content,
+      true
+    );
+
+    cloneImageOfBestSellingProducts.querySelector("img").src = image;
+
+    bestSellingProductsContainer.append(cloneImageOfBestSellingProducts);
+  });
+};
+
+let btnContainer = document.querySelector(".bestselling");
+let prevBtn = btnContainer.querySelector(".prev");
+let nextBtn = btnContainer.querySelector(".next");
+
+nextBtn.onclick = () => {
+  bestSellingProductsContainer.scrollLeft += 250;
+};
+prevBtn.onclick = () => {
+  bestSellingProductsContainer.scrollLeft -= 250;
+};
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import "./style.css";
 import "./Media-Query.css";
 import showSliderImage from "./imageSlider";
+import bestSellingProducts from "./bestselling.json";
+import { showBestSellingProduct } from "./bestSellingProducts";
 
 showSliderImage();
+
+showBestSellingProduct(bestSellingProducts);
+
